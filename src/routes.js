2737c0358fccdb80b385ff488e186f8c5f19606c
Edvaldo Mantovanelli <ecmantovanelli@gmail.com>
2019-07-14 21:04:22 -0300

    post user

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,10 @@
 import { Router } from 'express';
 
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Edvaldo Mantovanelli',
-    email: 'edvaldo@email.com',
-    password_hash: '123123123123',
-  });
-
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
+routes.get('/users', UserController.index);
 
 export default routes;
