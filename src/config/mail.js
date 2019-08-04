6f823325203351b901509c3c2aa281b0ebe9673e
Edvaldo Mantovanelli <ecmantovanelli@gmail.com>
2019-08-04 13:55:01 -0300

    sending mails

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '8ea0fc4a67cd2e',
+    pass: '128956a51604e8',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@teste.com>',
+  },
+};
