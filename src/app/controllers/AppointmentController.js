45ae94ee7d7bc6b3915786bf50a1bf334002a480
Edvaldo Mantovanelli <ecmantovanelli@gmail.com>
2019-08-04 09:02:59 -0300

    cancel appointment

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,6 +1,7 @@
 import * as Yup from 'yup';
-import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import { startOfHour, parseISO, isBefore, format, subHours } from 'date-fns';
 import pt from 'date-fns/locale/pt';
+// import { subHours } from 'date-fns/esm';
 import Notification from '../schemas/Notification';
 import Appointment from '../models/Appointment';
 import File from '../models/File';
@@ -9,6 +10,7 @@ import User from '../models/User';
 class AppointmentController {
   async index(req, res) {
     const { page = 1 } = req.query;
+
     const appointments = await Appointment.findAll({
       where: { user_id: req.userId, canceled_at: null },
       order: ['date'],
@@ -110,6 +112,30 @@ class AppointmentController {
       content: `Novo agendamento de ${user.name} para o ${formatedDate}`,
       user: provider_id,
     });
+
+    return res.json(appointment);
+  }
+
+  async delete(req, res) {
+    const appointment = await Appointment.findByPk(req.params.id);
+
+    if (appointment.user_id !== req.userId) {
+      return res.status(401).json({
+        error: "You don't have permission to cancel this appointment.",
+      });
+    }
+
+    const dateWithSub = subHours(appointment.date, 2);
+    // const dateWithSub = new Date();
+
+    if (isBefore(dateWithSub, new Date())) {
+      return res.status(401).json({
+        error: 'You can only cancel appointments 2 hours in advance',
+      });
+    }
+
+    appointment.canceled_at = new Date();
+    await appointment.save();
 
     return res.json(appointment);
   }
